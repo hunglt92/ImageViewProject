6cc794f889cb91ee2a10d502529c61def2e717c7
Hung FPT <hungtrong@Hungs-MacBook-Pro.local>
2013-06-18 01:39:38 +0700

    version 0.1

---
+++ ImageViewProject/ImageViewProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageViewProject
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageViewProject/ImageViewProjectTests/ImageViewProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  ImageViewProjectTests.m
+//  ImageViewProjectTests
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImageViewProjectTests : XCTestCase
+
+@end
+
+@implementation ImageViewProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
