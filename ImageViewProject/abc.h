f45f9f57203c5007473ce66d553273cb1a8fa393
Hung FPT <hungtrong@Hungs-MacBook-Pro.local>
2013-06-18 01:47:06 +0700

    santa

---
+++ ImageViewProject/abc.h
@@ -0,0 +1,13 @@
+//
+//  abc.h
+//  ImageViewProject
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface abc : NSObject
+
+@end
---
+++ ImageViewProject/abc.m
@@ -0,0 +1,13 @@
+//
+//  abc.m
+//  ImageViewProject
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import "abc.h"
+
+@implementation abc
+
+@end
